0d34350e8f0e0208564b3f6bea53474421127bfc
Kévin Gomez <kevin.gomez@grafana.com>
2024-05-27 15:04:02 +0200

    Update go module name

--- pkg/main.go
+++ pkg/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/grafana/grafana-as-code/pkg/plugin"
+	"github.com/grafana/grafana-cat-plugin/pkg/plugin"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/app"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
