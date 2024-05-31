aefc1ad0d52be6eded0547998d271663d9baf727
spinillos <selenepinillos@gmail.com>
2024-05-31 16:39:18 +0200

    Add filtering in response

--- src/hooks/resourceTypes.tsx
+++ src/hooks/resourceTypes.tsx
@@ -7,11 +7,9 @@ export const getResourceTypes = async(callback: any) => {
     try {
         const types = await getBackendSrv().get<ResourceTypeResponse>(`api/plugins/${pluginJson.id}/resources/resource-types`)
         const typeList: ResourceType[] = []
-        types.resources.forEach(type=>{
-            if (type.hasLister) {
-                type.selected = true
-                typeList.push(type)
-            }
+        types.resources.filter(res => res.hasLister).forEach(type=>{
+            type.selected = true
+            typeList.push(type)
         })
         console.log("TYPES", typeList)
         callback(typeList)
