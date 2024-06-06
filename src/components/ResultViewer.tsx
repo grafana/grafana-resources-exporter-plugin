f1732f61d4419c75fb5f2e5cf2eb0e17ce5d3181
Julien Duchesne <julien.duchesne@grafana.com>
2024-06-06 12:12:43 -0400

    ResultViewer: Determine format based on file extension Currently, it's hardcoded to hcl

--- src/components/ResultViewer.tsx
+++ src/components/ResultViewer.tsx
@@ -5,36 +5,50 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { GeneratedFile } from '../types/generator'
 
 interface ResultViewerProps {
-    files: GeneratedFile[]
-    language: string
+  files: GeneratedFile[]
+}
+
+const fileToLanguage = (file: string): string => {
+  switch (file.split(".").pop()) {
+    case "json":
+      return "json";
+    case "tf":
+    case "hcl":
+      return "hcl";
+    case "yml":
+    case "yaml":
+      return "yaml";
+    default:
+      return "plaintext";
+  }
 }
 
 export const ResultViewer = (props: ResultViewerProps) => {
-    const s = useStyles2(getStyles);
+  const s = useStyles2(getStyles);
 
-    const [activeTab, setActiveTab] = useState(0)
-    return (
-        <div className={s.marginTop}>
-          <TabsBar>
-            {props.files.map((file, i) => (<Tab key={i} active={i === activeTab} label={file.name} onChangeTab={_ => { setActiveTab(i) }} />))}
-          </TabsBar>
-          <TabContent>
-            <CodeEditor
-              width="100%"
-              height="500px"
-              value={props.files[activeTab].content}
-              language={props.language}
-              showLineNumbers={true}
-              showMiniMap={true}
-              readOnly={true}
-            />
-          </TabContent>
-        </div>
-      );
+  const [activeTab, setActiveTab] = useState(0)
+  return (
+    <div className={s.marginTop}>
+      <TabsBar>
+        {props.files.map((file, i) => (<Tab key={i} active={i === activeTab} label={file.name} onChangeTab={_ => { setActiveTab(i) }} />))}
+      </TabsBar>
+      <TabContent>
+        <CodeEditor
+          width="100%"
+          height="500px"
+          value={props.files[activeTab].content}
+          language={fileToLanguage(props.files[activeTab].name)}
+          showLineNumbers={true}
+          showMiniMap={true}
+          readOnly={true}
+        />
+      </TabContent>
+    </div>
+  );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-    marginTop: css`
+  marginTop: css`
       margin-top: ${theme.spacing(2)};
     `,
 });
