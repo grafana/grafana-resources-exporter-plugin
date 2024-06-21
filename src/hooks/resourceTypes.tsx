74f45218f60881dcbde6b96b347dcd9a611d402a
Julien Duchesne <julienduchesne@live.com>
2024-06-21 08:17:05 -0400

    Remember resource type selection when changing options (#34)
    Depends on https://github.com/grafana/grafana-resources-exporter-plugin/pull/30
    Currently, when changing from one format to another, the resource types are fetched again
    This resets the selection completely. With this PR, the selection is kept, unless the types change completely (ex: going from TF to Grizzly or vice versa)

--- src/hooks/resourceTypes.tsx
+++ src/hooks/resourceTypes.tsx
@@ -3,15 +3,27 @@ import { getBackendSrv } from '@grafana/runtime';
 import pluginJson from "../plugin.json"
 import { ResourceType, ResourceTypeResponse } from "../types/resourceTypes";
 
-export const getResourceTypes = async (target: string, outputFormat: string, callback: any) => {
+export const getResourceTypes = async (target: string, outputFormat: string, currentResourceTypes: ResourceType[], callback: any) => {
     try {
         const types = await getBackendSrv().get<ResourceTypeResponse>(`api/plugins/${pluginJson.id}/resources/resource-types`, { target: target, outputFormat: outputFormat })
-        const typeList: ResourceType[] = []
-        types.resources.forEach(type => {
-            type.selected = true
-            typeList.push(type)
+
+        // Re-select the resource types that were previously selected
+        // If the names don't match, reset to all selected
+        const currentResourceTypesMap = new Map<string, ResourceType>()
+        let matchedName = false
+        currentResourceTypes.forEach(type => {
+            currentResourceTypesMap.set(type.name, type)
         })
-        callback(typeList)
+        types.resources.forEach(t => {
+            if (currentResourceTypesMap.has(t.name)) {
+                matchedName = true
+                t.selected = currentResourceTypesMap.get(t.name)!.selected
+            }
+        })
+        if (!matchedName) {
+            types.resources.forEach(t => t.selected = true)
+        }
+        callback(types.resources)
     } catch (err) {
         console.log(err)
     }
