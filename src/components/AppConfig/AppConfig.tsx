grafana/grafana-resources-exporter-plugin

commit 74f45218f60881dcbde6b96b347dcd9a611d402a

Remember resource type selection when changing options (#34)
Depends on https://github.com/grafana/grafana-resources-exporter-plugin/pull/30
Currently, when changing from one format to another, the resource types are fetched again
This resets the selection completely. With this PR, the selection is kept, unless the types change completely (ex: going from TF to Grizzly or vice versa)

src/hooks/resourceTypes.tsx
@@ -3,15 +3,27 @@ import { getBackendSrv } from '@grafana/runtime';
 import pluginJson from "../plugin.json"
 import { ResourceType, ResourceTypeResponse } from "../types/resourceTypes";
 
-export const getResourceTypes = async (target: string, outputFormat: string, callback: any) => {
+export const getResourceTypes = async (target: string, outputFormat: string, currentResourceTypes: ResourceType[], callback: any) => {
     try {
         const types = await getBackendSrv().get<ResourceTypeResponse>(`api/plugins/${pluginJson.id}/resources/resource-types`, { target: target, outputFormat: outputFormat })
-        const typeList: ResourceType[] = []
-        types.resources.forEach(type => {
-            type.selected = true
-            typeList.push(type)
+
+        // Re-select the resource types that were previously selected
+        // If the names don't match, reset to all selected
+        const currentResourceTypesMap = new Map<string, ResourceType>()
+        let matchedName = false
+        currentResourceTypes.forEach(type => {
+            currentResourceTypesMap.set(type.name, type)
         })
-        callback(typeList)
+        types.resources.forEach(t => {
+            if (currentResourceTypesMap.has(t.name)) {
+                matchedName = true
+                t.selected = currentResourceTypesMap.get(t.name)!.selected
+            }
+        })
+        if (!matchedName) {
+            types.resources.forEach(t => t.selected = true)
+        }
+        callback(types.resources)
     } catch (err) {
         console.log(err)
     }

src/components/OptionsSelector.tsx
@@ -44,9 +44,8 @@ export function OptionsSelector(props: OptionsSelectorProps) {
   }, [target, format])
 
   useEffect(() => {
-    console.log("GETTING RESOURCE TYPES")
-    getResourceTypes(target, format, setResourceTypes)
-  }, [target, format]);
+    getResourceTypes(target, format, resourceTypes, setResourceTypes)
+  }, [target, format, resourceTypes]);
 
   useEffect(() => {
     if (resourceTypes.filter(r => r.selected).length === 0) {

commit 6b980f78f2779e3b915040e41e1d62928ab6c986

Fix infinite loop of resource type reloading Bug introduced in https://github.com/grafana/grafana-resources-exporter-plugin/pull/34 The resourceTypes are being set in a loop, because we pass in the previously selected resource types, and we set a new list on every function call In order to break this loop, I add a check so that we don't call the setter if the old value is the same as the new one
I also add a linting check so that we don't have `any` anywhere in the code

.eslintrc
@@ -1,3 +1,6 @@
 {
-  "extends": "./.config/.eslintrc"
+  "extends": "./.config/.eslintrc",
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "error"
+  }
 }

src/components/AppConfig/AppConfig.tsx
@@ -210,7 +210,7 @@ export const AppConfig = ({ plugin }: AppConfigProps) => {
             type="submit"
             data-testid={testIds.appConfig.submit}
             onClick={() => {
-              const secureJsonData: KeyValue<any> = {};
+              const secureJsonData: KeyValue<string> = {};
               if (!state.isServiceAccountTokenSet) {
                 secureJsonData.serviceAccountToken = state.serviceAccountToken;
               }

src/components/ResourceTypeSelector.tsx
@@ -6,7 +6,7 @@ import { ResourceType } from '../types/resourceTypes'
 
 interface ResourceTypeSelectorProps {
   resourceTypes: ResourceType[]
-  onChange: any
+  onChange: (resourceTypes: ResourceType[]) => void
 }
 
 export function ResourceTypeSelector(props: ResourceTypeSelectorProps) {
@@ -29,9 +29,10 @@ export function ResourceTypeSelector(props: ResourceTypeSelectorProps) {
   }
 
   const grouped = props.resourceTypes.reduce(
-    (result: any, currentValue: any) => {
-      (result[currentValue['category']] = result[currentValue['category']] || []).push(currentValue);
-      result[currentValue['category']] = result[currentValue['category']].sort((a: ResourceType, b: ResourceType) => (a.name.localeCompare(b.name)));
+    (result: Record<string, ResourceType[]>, currentValue: ResourceType) => {
+      const categoryResult = result[currentValue.category] || [];
+      categoryResult.push(currentValue);
+      result[currentValue.category] = categoryResult.sort((a: ResourceType, b: ResourceType) => (a.name.localeCompare(b.name)));
       return result;
     }, {});
   return <div>

src/hooks/resourceTypes.tsx
@@ -3,24 +3,34 @@ import { getBackendSrv } from '@grafana/runtime';
 import pluginJson from "../plugin.json"
 import { ResourceType, ResourceTypeResponse } from "../types/resourceTypes";
 
-export const getResourceTypes = async (target: string, outputFormat: string, currentResourceTypes: ResourceType[], callback: any) => {
+export const getResourceTypes = async (
+    target: string,
+    outputFormat: string,
+    currentResourceTypes: ResourceType[],
+    callback: (newResourceTypes: ResourceType[]) => void
+) => {
     try {
         const types = await getBackendSrv().get<ResourceTypeResponse>(`api/plugins/${pluginJson.id}/resources/resource-types`, { target: target, outputFormat: outputFormat })
 
         // Re-select the resource types that were previously selected
         // If the names don't match, reset to all selected
         const currentResourceTypesMap = new Map<string, ResourceType>()
-        let matchedName = false
+        let matchedNameCount = 0
         currentResourceTypes.forEach(type => {
             currentResourceTypesMap.set(type.name, type)
         })
         types.resources.forEach(t => {
             if (currentResourceTypesMap.has(t.name)) {
-                matchedName = true
+                matchedNameCount += 1
                 t.selected = currentResourceTypesMap.get(t.name)!.selected
             }
         })
-        if (!matchedName) {
+
+        if (matchedNameCount === types.resources.length) {
+            return // Do nothing, types are the same. This avoids an infinite loop where resourceTypes keeps being set to the same value
+        }
+
+        if (matchedNameCount === 0) {
             types.resources.forEach(t => t.selected = true)
         }
         callback(types.resources)