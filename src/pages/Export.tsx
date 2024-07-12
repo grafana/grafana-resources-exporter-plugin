f485b54e12c6d015db24e72c27e52f2eafeef5b8
Julien Duchesne <julien.duchesne@grafana.com>
2024-07-12 14:37:35 -0400

    Add support for non-critical errors If a single resource fails to export, it shouldn't crash the whole process

--- src/pages/Export.tsx
+++ src/pages/Export.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { PluginPage, getBackendSrv, isFetchError } from '@grafana/runtime';
-import { ErrorWithStack, ToolbarButton, ToolbarButtonRow } from '@grafana/ui';
+import { Alert, ErrorWithStack, ToolbarButton, ToolbarButtonRow } from '@grafana/ui';
 import { GeneratedFile, GenerateRequest, GenerateResponse } from "../types/generator";
 import { saveAs } from 'file-saver';
 import JSZip from "jszip";
@@ -11,7 +11,8 @@ import { OptionsSelector } from 'components/OptionsSelector';
 export function ExportPage() {
   const [files, setFiles] = useState<GeneratedFile[]>([])
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<{ title: string, error: Error, info: React.ErrorInfo | null } | undefined>(undefined)
+  const [error, setError] = useState<{ title: string, error: Error } | undefined>(undefined)
+  const [warning, setWarning] = useState<{ title: string, error: Error } | undefined>(undefined)
   const [options, setOptions] = useState<GenerateRequest | undefined>(undefined)
   const [optionsCollapsed, setOptionsCollapsed] = useState(false)
 
@@ -43,7 +44,7 @@ export function ExportPage() {
   </div>
 
   if (error) {
-    content = <ErrorWithStack title={error.title} error={error.error} errorInfo={error.info} />;
+    content = <ErrorWithStack title={error.title} error={error.error} errorInfo={null} />;
   } else if (files.length > 0) {
     content = <ResultViewer height={editorHeight + 'px'} files={files} />
   } else if (loading) {
@@ -55,14 +56,17 @@ export function ExportPage() {
     try {
       const exports = await getBackendSrv().post<GenerateResponse>(`api/plugins/${pluginJson.id}/resources/generate`, options, { showErrorAlert: false });
       setFiles(exports.files || [{ name: "Result", content: "No resources were found" }])
+      if (exports.warnings.length > 0) {
+        setWarning({ title: "Some resources could not be exported", error: new Error(exports.warnings.join('\n')) })
+      }
       setError(undefined)
     } catch (err) {
       if (err instanceof Error) {
-        setError({ title: err.message, error: err, info: null })
+        setError({ title: err.message, error: err })
       } else if (isFetchError(err)) {
-        setError({ title: err.statusText!, error: new Error(err.data?.message), info: null })
+        setError({ title: err.statusText!, error: new Error(err.data?.message) })
       } else {
-        setError({ title: "Unexpected Error", error: new Error(`${err}`), info: null })
+        setError({ title: "Unexpected Error", error: new Error(`${err}`) })
       }
     }
     setLoading(false)
@@ -95,6 +99,11 @@ export function ExportPage() {
 
   return (
     <PluginPage actions={actions}>
+      {warning &&
+        <Alert title={warning.title} onRemove={() => setWarning(undefined)}>
+          {warning.error.message}
+        </Alert>
+      }
       <div ref={topRef} style={optionsCollapsed ? { display: 'none' } : {}}>
         <OptionsSelector onChange={setOptions} />
       </div>
