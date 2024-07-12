c9ca241b6aab8627e3970c7058792583290d144e
Julien Duchesne <julien.duchesne@grafana.com>
2024-07-11 22:41:47 -0400

    Layout: Use toolbar buttons Without redoing the whole thing, this saves the most space and is the best I could do
    We can rethink the options selector once it's release I think. After we get feedback. For now, it's functional

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,3 @@ import pluginJson from './plugin.json';
 
 export const PLUGIN_BASE_URL = `/a/${pluginJson.id}`;
 
-export enum ROUTES {
-  One = 'one',
-  Two = 'two',
-  Three = 'three',
-  Four = 'four',
-  Export = 'export',
-  Start = 'start',
-}
